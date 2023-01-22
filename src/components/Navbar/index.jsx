0dac21bbcd7fb1d6e6711bbb3162d267c143aa3a
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-01-22 01:17:02 -0300

    First commit

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,65 @@
+import {
+  Box,
+  Flex,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  useColorModeValue,
+  Stack,
+  Input,
+  InputGroup,
+  InputLeftElement
+} from '@chakra-ui/react'
+import { HamburgerIcon, SearchIcon } from '@chakra-ui/icons'
+import ToggleColorMode from '../toggleThemeButton'
+import { Form } from 'react-router-dom'
+import Footprint from '../Footprint'
+
+export default function Navbar() {
+  return (
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <Box display={'inline-flex'} alignItems="center">
+          <Footprint />
+          Sebass
+        </Box>
+        <Flex alignItems={'center'}>
+          <Stack direction={'row'} spacing={7}>
+            <Box display={'inline-flex'} alignItems="center">
+              <InputGroup>
+                <InputLeftElement opacity={'40%'}>
+                  <Button
+                    type="submit"
+                    bg={'none'}
+                    _hover={{ background: 'none' }}
+                  >
+                    <SearchIcon />
+                  </Button>
+                </InputLeftElement>
+                <Input placeholder="Search here!" />
+              </InputGroup>
+            </Box>
+            <ToggleColorMode />
+            <Menu>
+              <MenuButton
+                as={Button}
+                rounded={'7'}
+                variant={'link'}
+                border="1px"
+              >
+                <HamburgerIcon />
+              </MenuButton>
+              <MenuList alignItems={'center'}>
+                <MenuItem>Your Servers</MenuItem>
+                <MenuItem>Account Settings</MenuItem>
+                <MenuItem>Logout</MenuItem>
+              </MenuList>
+            </Menu>
+          </Stack>
+        </Flex>
+      </Flex>
+    </Box>
+  )
+}
---
+++ src/theme.jsx
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = {
+  config: {
+    intialColorMode: 'dark',
+    disableTransitionOnChange: false,
+    useSystemColorMode: true
+  }
+}
+
+export default extendTheme(theme)
