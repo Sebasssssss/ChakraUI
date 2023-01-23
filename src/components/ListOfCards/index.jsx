e35115a44b7e29e350f8c7ac284211f76b901461
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-01-23 17:07:51 -0300

    Updates

--- src/components/ListOfCards/index.jsx
+++ src/components/ListOfCards/index.jsx
@@ -7,34 +7,34 @@ export default function ListOfCards() {
       <Link to="/page2" className="w-full mb-4 text-center">
         <img
           src="https://i.pinimg.com/564x/5e/2b/6b/5e2b6b57e68519bee6c9db90eeb18089.jpg"
-          className="w-full border border-slate-300 dark:border-slate-700 rounded-xl h-36 object-cover"
+          className="w-full border border-slate-300 dark:border-slate-700 rounded-xl h-32 object-cover"
         />
-        <h6 className="text-xl mt-3 font-bold">Title</h6>
-        <h1 className="opacity-70">Subtitle</h1>
+        <h6 className="text-md mt-3 font-mplus font-bold">Title</h6>
+        <h1 className="text-sm opacity-70">Subtitle</h1>
       </Link>
       <Link to="/page2" className="w-full mb-4 text-center">
         <img
           src="https://i.pinimg.com/564x/5e/2b/6b/5e2b6b57e68519bee6c9db90eeb18089.jpg"
-          className="w-full border border-slate-300 dark:border-slate-700 rounded-xl h-36 object-cover"
+          className="w-full border border-slate-300 dark:border-slate-700 rounded-xl h-32 object-cover"
         />
-        <h6 className="text-xl mt-3 font-bold">Title</h6>
-        <h1 className="opacity-70">Subtitle</h1>
+        <h6 className="text-md mt-3 font-mplus font-bold">Title</h6>
+        <h1 className="text-sm opacity-70">Subtitle</h1>
       </Link>
       <Link to="/page2" className="w-full mb-4 text-center">
         <img
           src="https://i.pinimg.com/564x/5e/2b/6b/5e2b6b57e68519bee6c9db90eeb18089.jpg"
-          className="w-full border border-slate-300 dark:border-slate-700 rounded-xl h-36 object-cover"
+          className="w-full border border-slate-300 dark:border-slate-700 rounded-xl h-32 object-cover"
         />
-        <h6 className="text-xl mt-3 font-bold">Title</h6>
-        <h1 className="opacity-70">Subtitle</h1>
+        <h6 className="text-md mt-3 font-mplus font-bold">Title</h6>
+        <h1 className="text-sm opacity-70">Subtitle</h1>
       </Link>
       <Link to="/page2" className="w-full mb-4 text-center">
         <img
           src="https://i.pinimg.com/564x/5e/2b/6b/5e2b6b57e68519bee6c9db90eeb18089.jpg"
-          className="w-full border border-slate-300 dark:border-slate-700 rounded-xl h-36 object-cover"
+          className="w-full border border-slate-300 dark:border-slate-700 rounded-xl h-32 object-cover"
         />
-        <h6 className="text-xl mt-3 font-bold">Title</h6>
-        <h1 className="opacity-70">Subtitle</h1>
+        <h6 className="text-md mt-3 font-mplus font-bold">Title</h6>
+        <h1 className="text-sm opacity-70">Subtitle</h1>
       </Link>
     </div>
   )
