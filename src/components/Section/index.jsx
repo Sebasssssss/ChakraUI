a8f2c9d490bcced24bf0fa169c7d96f9c0ca9504
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-01-28 01:16:07 -0300

    Section component edited

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { chakra } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
+
+const ChakraBox = chakra(motion.div, {
+  shouldForwardProp: isValidMotionProp
+})
 
 export default function Section({ children, delay = 0 }) {
   return (
-    <motion.div
+    <ChakraBox
       initial={{ y: 10, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.8, delay }}
     >
       {children}
-    </motion.div>
+    </ChakraBox>
   )
 }
