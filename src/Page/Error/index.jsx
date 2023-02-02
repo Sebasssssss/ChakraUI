5a36756dd58b275bfa625d22c8eb5c238262e80f
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-02-02 12:03:03 -0300

    Updates

--- src/Page/Error/index.jsx
+++ src/Page/Error/index.jsx
@@ -3,7 +3,7 @@ import { Box, Text } from '@chakra-ui/react'
 export default function NotFound() {
   return (
     <Box display="flex" flexDir="column" gap="-5">
-      <Text mt="8" fontSize="7xl" className="font-mplus">
+      <Text mt="8" fontSize="5xl" className="font-mplus">
         Got lost?
       </Text>
       <Text className="font-mplus">404 Not found</Text>
